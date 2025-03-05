deb18464e8a3220d30fa591f43ca430f7001a562
PietroThUser <141967287+PietroThUser@users.noreply.github.com>
2025-03-05 19:41:51 -0300

    Update main.py

--- enc-tech_directory/main.py
+++ enc-tech_directory/main.py
@@ -1,69 +1,20 @@
 from flask import Flask, render_template
-from bs4 import BeautifulSoup
 from gunicorn import app
+from formatter import formatter
 
 app = Flask(__name__)
 
-def HTML_CONVERTER(conteudo):
-    soup = BeautifulSoup(features="html.parser")
-    linhas = conteudo.split("\n")
-    ul_atual = None  # Controla a UL atual
-
-    for linha in linhas:
-        linha = linha.strip()
-        
-        if linha.startswith("### "):
-            h3 = soup.new_tag("h3")
-            h3.string = linha.replace("### ", "", 1).strip()
-            soup.append(h3)
-            ul_atual = None  # Reseta ao criar novo cabeçalho
-
-        elif linha.startswith("## "):
-            h2 = soup.new_tag("h2")
-            h2.string = linha.replace("## ", "", 1).strip()
-            soup.append(h2)
-            ul_atual = None
-
-        elif linha.startswith("# "):
-            h1 = soup.new_tag("h1")
-            h1.string = linha.replace("# ", "", 1).strip()
-            soup.append(h1)
-            ul_atual = None
-
-        elif linha.startswith("- "):
-            li = soup.new_tag("li")
-            li.string = linha.replace("- ", "", 1).strip()
-            
-            if not ul_atual:  # Cria nova UL se não existir
-                ul_atual = soup.new_tag("ul")
-                soup.append(ul_atual)
-                
-            ul_atual.append(li)
-
-        elif linha == "---":
-            hr = soup.new_tag("hr")
-            soup.append(hr)
-            ul_atual = None
-
-        elif linha:
-            p = soup.new_tag("p")
-            p.string = linha
-            soup.append(p)
-            ul_atual = None
-
-    return soup.prettify()
-
 @app.route("/")
 def hub_introduction():
-    conteudo_convertido = []
+    converted_content = []
 
     for i in range(1, 4):
-        with open(f"templates/TEXTO{i}.txt", "r") as arquivo:
-            conteudo = arquivo.read()
-            HTML_CONVERTER_VAR = HTML_CONVERTER(conteudo)
-            conteudo_convertido.append(HTML_CONVERTER_VAR)
+        with open(f"templates/TEXTO{i}.txt", "r") as file:
+            content = file.read()
+            formatter_var = formatter(content)
+            converted_content.append(formatter_var)
 
-    return render_template('index.html', textos=conteudo_convertido)
+    return render_template('index.html', texts=converted_content)
 
-if  __name__ == '__main__':
+if __name__ == '__main__':
     app.run()
